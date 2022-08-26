c6b602fb1d62cb615c9287d9dcd4a3fc93465b44
platanob <102884156+platanob@users.noreply.github.com>
2022-08-25 20:17:33 -0400

    Add files via upload

---
+++ Proyecto/Nueva carpeta (2)/js.js
@@ -0,0 +1,9 @@
+
+function fun() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    msj = document.getElementById("mensaje").value;
+    alert("Tu nombre es: " + nombre + "\n" +
+        "Tu email es:" + email + "\n" +
+        "Tu mensaje: " + msj);
+}
