bfb2de333f68998088d3fd423a18c9e9853ce8b6
platanob <102884156+platanob@users.noreply.github.com>
2022-08-25 20:21:50 -0400

    Delete Proyecto directory

--- Proyecto/Nueva carpeta (2)/js.js
+++
@@ -1,9 +0,0 @@
-
-function fun() {
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    msj = document.getElementById("mensaje").value;
-    alert("Tu nombre es: " + nombre + "\n" +
-        "Tu email es:" + email + "\n" +
-        "Tu mensaje: " + msj);
-}
